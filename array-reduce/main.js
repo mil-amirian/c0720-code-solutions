1e275b0c1806deca28fd6d4b1ab5b36cfd8e5ee8
Mil Amirian <milamirian22@gmail.com>
2020-09-02 08:17:14 -0700

    completed exercise

---
+++ array-reduce/main.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-console, no-unused-vars */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+
+const sum = (number, currentValue) => number + currentValue;
+console.log(numbers.reduce(sum));
+
+const product = (number, currentValue) => number * currentValue;
+console.log(numbers.reduce(product));
+
+const initialValue = 0;
+
+const groupBy = (objectArray, property) => {
+  return objectArray.reduce((acc, obj) => {
+    const key = obj[property];
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+  }, {});
+};
+
+const accountTrans = groupBy(account, 'type');
+
+const calcs = (number, currentValue) => number + currentValue.amount;
+const deposit = accountTrans.deposit.reduce((calcs), initialValue);
+const withdrawal = accountTrans.withdrawal.reduce((calcs), initialValue);
+const accountBalance = deposit - withdrawal;
+console.log(accountBalance);
+
+const composite = {};
+Object.assign(composite, traits);
+console.log(composite);
