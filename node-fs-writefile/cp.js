156b701ca3a843f3b92b175f83efacec4e9754b2
Mil Amirian <milamirian22@gmail.com>
2020-09-04 11:12:38 -0700

    completed section 3 rework

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
 
 const fs = require('fs');
+const newFileName = process.argv[3];
 
 fs.readFile(process.argv[2], 'utf8', (err, info) => {
   if (err) throw err;
   const copy = new Uint8Array(Buffer.from(info));
-  fs.writeFile('reminder.txt', copy, err => {
+  fs.writeFile(newFileName, copy, err => {
     if (err) throw err;
   });
 });
