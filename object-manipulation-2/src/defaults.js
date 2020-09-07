e89c4dfd6726b4aad4e0e47ce8d510dc1931ac04
Mil Amirian <milamirian22@gmail.com>
2020-09-06 20:50:57 -0700

    Completed Exercise

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+const defaults = (target, source) => {
+  const targetKeys = Object.keys(target);
+  const sourceKeys = Object.keys(source);
+  for (let i = 0; i < sourceKeys.length; i++) {
+    if (sourceKeys[i] !== targetKeys[i]) {
+      target[sourceKeys[i]] = source[sourceKeys[i]];
+    }
+  }
+  return target;
+};
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+const pick = (source, keys) => {
+  const resultObj = {};
+  for (let i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      resultObj[keys[i]] = source[keys[i]];
+    }
+  }
+  return resultObj;
+};
