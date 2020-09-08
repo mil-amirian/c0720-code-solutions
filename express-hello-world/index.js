a319ddb7c82495a495004e4ce8865349fd3445a8
Mil Amirian <milamirian22@gmail.com>
2020-09-08 15:54:12 -0700

    Completed Express Hello World Exercise

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -3,10 +3,13 @@ const express = require('./node_modules/express/index.js');
 const app = express();
 
 app.use(function (req, res, next) {
-  res.send('Hello, World!');
+  // eslint-disable-next-line no-console
+  console.log(req.method);
+  res.send('<h1>Hello, World!</h1>');
   next();
 });
 
 app.listen(3000, () => {
-  print('Server is running on port 3000');
+  // eslint-disable-next-line no-console
+  console.log('Server is running on port 3000');
 });
