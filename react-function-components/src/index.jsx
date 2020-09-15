cca599e73b8c2941a066ab4535bb20a79820d567
Mil Amirian <milamirian22@gmail.com>
2020-09-15 19:56:59 +0000

    Working now

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function CustomButton() {
-  return <button>Click Me!</button>;
+function CustomButton(props) {
+  return <button>{props.buttonText}</button>;
 }
 
 const htmlElement = document.getElementById('root');
 
-ReactDOM.render(<CustomButton />, htmlElement);
+ReactDOM.render(<CustomButton buttonText="Click Me!"/>, htmlElement);
