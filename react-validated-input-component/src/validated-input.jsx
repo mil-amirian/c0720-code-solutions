3a62b4a42e81c0afe4f4e303fcf581ddf63a1f0a
Mil Amirian <milamirian22@gmail.com>
2020-09-18 16:12:46 +0000

    revised code per feedback

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -9,6 +9,9 @@ class ValidateInput extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.NoMoreRepeatedCode = this.NoMoreRepeatedCode.bind(this);
+    this.ConditionalStatuses = this.ConditionalStatuses.bind(this);
+    this.ConditionalImages = this.ConditionalImages.bind(this);
   }
 
   handleSubmit(event) {
@@ -34,69 +37,74 @@ class ValidateInput extends React.Component {
     }
   }
 
-  render() {
+  NoMoreRepeatedCode() {
+    return (
+      <form className="container" onSubmit={this.handleSubmit}>
+        <label>Password</label>
+        <div className="input-container">
+          <input type="password" className="input" value={this.state.handleSubmit} onChange={this.handleChange}/>
+          <input type="submit" value="Sign In"/>
+          <this.ConditionalImages />
+        </div>
+        <this.ConditionalStatuses />
+      </form>
+    );
+  }
+
+  ConditionalStatuses() {
     if (this.state.entry === 'empty') {
       return (
-        <form className="container" onSubmit={this.handleSubmit}>
-          <label>Password</label>
-          <div className="input-container">
-            <input type="password" className="input" value={this.state.handleSubmit} onChange={this.handleChange}/>
-            <input type="submit" value="Sign In"/>
-            <img src="./wrong.png" className="wrong" />
-            <img src="./right.png" className="right hidden"/>
-          </div>
-
+        <>
           <div className="status-wrong">A password is required</div>
           <div className="status-short hidden">Your password is too short</div>
-        </form>
+        </>
       );
     } else if (this.state.entry === 'short') {
       return (
-        <form className="container" onSubmit={this.handleSubmit}>
-          <label>Password</label>
-          <div className="input-container">
-            <input type="password" className="input" value={this.state.handleSubmit} onChange={this.handleChange}/>
-            <input type="submit" value="Sign In"/>
-            <img src="./wrong.png" className="wrong" />
-            <img src="./right.png" className="right hidden"/>
-          </div>
-
+        <>
           <div className="status-wrong hidden">A password is required</div>
           <div className="status-short">Your password is too short</div>
-        </form>
+        </>
       );
-    } else if (this.state.entry === 'pass') {
+    } else if (this.state.entry === 'pass' || this.state.entry === 'start') {
       return (
-        <form className="container" onSubmit={this.handleSubmit}>
-          <label>Password</label>
-          <div className="input-container">
-            <input type="password" className="input" value={this.state.handleSubmit} onChange={this.handleChange} />
-            <input type="submit" value="Sign In"/>
-            <img src="./wrong.png" className="wrong hidden" />
-            <img src="./right.png" className="right"/>
-          </div>
-
+        <>
           <div className="status-wrong hidden">A password is required</div>
           <div className="status-short hidden">Your password is too short</div>
-        </form>
-      );
-    } else {
-      return (
-        <form className="container" onSubmit={this.handleSubmit}>
-          <label>Password</label>
-          <div className="input-container">
-            <input type="password" className="input" value={this.state.handleSubmit} onChange={this.handleChange}/>
-            <input type="submit" value="Sign In"/>
-            <img src="./wrong.png" className="wrong hidden" />
-            <img src="./right.png" className="right hidden"/>
-          </div>
-
-          <div className="status-wrong hidden">A password is required</div>
-          <div className="status-short hidden">Your password is too short</div>
-        </form>
+        </>
       );
     }
 
+  }
+
+  ConditionalImages() {
+    if (this.state.entry === 'empty' || this.state.entry === 'short') {
+      return (
+        <>
+          <img src="./wrong.png" className="wrong" />
+          <img src="./right.png" className="right hidden"/>
+        </>
+      );
+    } else if (this.state.entry === 'pass') {
+      return (
+        <>
+          <img src="./wrong.png" className="wrong hidden" />
+          <img src="./right.png" className="right"/>
+        </>
+      );
+    } else if (this.state.entry === 'start') {
+      return (
+        <>
+          <img src="./wrong.png" className="wrong hidden" />
+          <img src="./right.png" className="right hidden"/>
+        </>
+      );
+
+    }
+  }
+
+  render() {
+    return <this.NoMoreRepeatedCode />;
   }
 }
 
